1ac33809f80959d86bcd5fdffcb5ebf056f342bc
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-29 04:56:22 +0530

    aksmaksa

---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+   public int age= 25;
+
+    void myMethod(){
+        System.out.println(age);
+    }
+}
---
+++ src/TechEx.java
@@ -0,0 +1,37 @@
+import groovy.console.ui.AstBrowser;
+
+public class TechEx {
+    public static void main(String[] args) {
+        dog d = new dog();
+        d.eat();
+        d.run();
+        d.play();
+
+        cat c = new cat();
+
+
+    }
+}
+ class Animal {
+     String color;
+
+
+     void run() {
+         System.out.println("Run");
+     }
+     void eat(){
+         System.out.println("Eat");
+     }
+ }
+
+ class dog extends Animal{
+
+    void play(){
+        System.out.println("Play");
+    }
+
+ }
+
+class cat extends Animal{
+
+}

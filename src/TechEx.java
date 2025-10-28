9faf94ca6d7b303a349c8c5f7985c44583858e46
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-29 05:21:08 +0530

    OOP Multi-Level Inheritance

--- src/TechEx.java
+++ src/TechEx.java
@@ -2,40 +2,29 @@ import groovy.console.ui.AstBrowser;
 
 public class TechEx {
     public static void main(String[] args) {
-        dog d = new dog();
-        d.eat();
-        d.run();
-        d.play();
-
-        cat c = new cat();
-
+     BMW bmw = new BMW();
+     bmw.driveFast();
+     bmw.drive();
+     bmw.racing();
 
     }
 }
- class Animal {
-     String color;
 
-
-     void run() {
-         System.out.println("Run");
-     }
-     void eat(){
-         System.out.println("Eat");
-     }
- }
-
- class dog extends Animal{
-
-    void play(){
-        System.out.println("Play");
+  class vehicle{
+    void drive(){
+        System.out.println("Drive");
     }
 
+  }
+
+ class car extends vehicle{
+    void racing(){
+        System.out.println("Racing");
+    }
  }
 
-class cat extends Animal{
-
-    void hunt(){
-
+class BMW extends car{
+    void driveFast(){
+        System.out.println("Drive Fast");
     }
-
 }

d7e1e19ddd3cc8e365b435ca4dbb1065749e46c2
HMGChamidu <chamindusandeepa339@gmail.com>
2025-11-17 13:48:02 +0530

    This method II

--- src/Day4/Main.java
+++ src/Day4/Main.java
@@ -2,16 +2,27 @@ package Day4;
 
 public class Main {
     public static void main(String[] args) {
-        Animal a = new Animal();
-        a.printName();
+        Animal a1 = new Animal("Dog");
+        Animal a2 = new Animal("Cat");
+        Animal a3 = new Animal("Fish");
+
+        a1.printName();
+        a2.printName();
+        a3.printName();
+
+
     }
 }
 
 class Animal{
-    String name = "Dog";
+    String name;
 
-  void printName(){
-      System.out.println(this.name);
-  }
+    public Animal (String _name){
+        name = _name;
+    }
+
+    void printName(){
+        System.out.println(this.name);
+    }
     
 }
